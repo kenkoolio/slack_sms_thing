6b2ca1419c1b1a1e5465aeba28aeb4e6458cccc6
Ken Nguyen <aznboikenny@yahoo.com>
2017-05-23 17:19:59 -0500

    how bou dah

--- server.py
+++ server.py
@@ -72,13 +72,13 @@ def relay_sms():
 def reply_to_slack():
     webhook_url = "https://hooks.slack.com/services/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"
     request_body = request.form.to_dict()
-    from_phone = json.dumps(request_body["From"])
+    from_phone = request_body["From"]
     message_body = json.dumps(request_body["Body"])
     print("from twilio:", from_phone, message_body)
 
     return_message = '{}: {}'.format(from_phone, message_body)
     return_body = {"text": return_message}
-    return_headers = {"Content-Type": "application/json"}
+    return_headers = '"Content-Type": "application/json"'
     requests.post(webhook_url, data = return_body, headers = return_headers)
     return "", 200
 
