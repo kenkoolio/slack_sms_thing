40a0f101a3d4512169353f346dafbb85340f3ccf
Ken Nguyen <aznboikenny@yahoo.com>
2017-05-23 16:56:32 -0500

    meow

--- server.py
+++ server.py
@@ -73,9 +73,9 @@ def reply_to_slack():
     webhook_url = "https://hooks.slack.com/services/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"
     request_body = request.form.to_dict()
     json_body = json.dumps(request_body)
-    # from_phone = request_body["From"]
-    # message_body = request_body["Body"]
-    print("from twilio:", json_body)
+    from_phone = json_body["From"]
+    message_body = json_body["Body"]
+    print("from twilio:", from_phone, message_body)
     return "", 200
 
 
