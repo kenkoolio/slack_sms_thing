9ed789c112b5e9f12510f28ec34b3f9d1f4ffea1
Ken Nguyen <aznboikenny@yahoo.com>
2017-05-22 22:38:15 -0500

    time to test full process

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, jsonify, abort
 from twilio.rest import Client
-from auth import sid, token
+from auth import sid, twilio_token, slack_token
 import requests
 
 app = Flask(__name__)
@@ -14,8 +14,8 @@ app.secret_key = 'secretsquirrel'
 #   channel_name=test
 #   user_id=U2147483697
 #   user_name=Steve
-#   command=/weather
-#   text=94070
+#   command=/smsout
+#   text="1234567890 message goes here"
 #   response_url=https://hooks.slack.com/commands/1234/5678
 
 #index
@@ -27,24 +27,32 @@ def index():
 #relay sms from slack to phone
 @app.route('/smsout', methods=['POST'])
 def relay_sms():
-    twilio_number = "+14698047301"
+    #verify request is from slack
+    slack_verify_token = request.form.get("token")
+    if not slack_verify_token == slack_token:
+        abort(400)
+
     #incoming_message = request.get_json(force=True)
 
-
+    #incoming slack data
     incoming_message = request.form.get("text")
-    slack_token = request.form.get("token")
 
-    # to = "+1"+incoming_message["to"]
-    # from_ = twilio_number
-    # body = incoming_message["body"]
-    # #todo callback URL
-    #
-    # client = Client(sid, token)
-    #
-    # client.messages.create(
-    #     to=to,
-    #     from_=from_,
-    #     body=body)
+    out_number = incoming_message[:10]
+    out_message = incoming_message[11:]
+
+    #twilio API to send slack message to mobile phone
+    twilio_number = "+14698047301"
+    to = "+1"+out_number
+    from_ = twilio_number
+    body = out_message
+    #todo callback URL
+
+    client = Client(sid, twilio_token)
+
+    client.messages.create(
+        to=to,
+        from_=from_,
+        body=body)
 
     print("look here", incoming_message, slack_token)
     # return render_template('index.html', message_data=incoming_message)
